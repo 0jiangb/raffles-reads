16416b071e5b74ab968b479dc5f1fbeaf77ab0ef
Jiang Bowen <0jiangb@gmail.com>
2018-04-15 10:02:24 +0800

    Added image support

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,7 +11,7 @@ export class HomePage {
 
   items: Array<Object>;
   constructor(private storage: AngularFireStorage) {
-    this.items = [{"header": "Hello", "shortForm": "hello2"}];
+    this.items = [{"header": "Hello", "shortForm": "hello2"},{"header": "Hello", "shortForm": "hello2", "imgSrc": "https://sms.math.nus.edu.sg/PrizePresentation/PhotoSMO2016/SMOJunior/content/images/large/2016_SMS_APPC_small-189.jpg"}];
   }
 
 }
